6028992dc3432a1797139b74ed94f3d8453128ff
Umar Ali <umeralikhan7864@gmail.com>
2024-01-20 23:11:20 +0500

    DOne

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import GithubIcon from "../../../public/github-icon.svg"
-import LinkedinIcon from "../../../public/linkedin-icon.svg"
+// import GithubIcon from "../../../public/github-icon.svg"
+// import LinkedinIcon from "../../../public/linkedin-icon.svg"
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
@@ -42,7 +42,7 @@ const Hero = () => {
                         </Button>
                     </div>
                     <div className="pl-6 py-4 gap-x-1 flex justify-center md:justify-start lg:gap-x-3">
-                        <Link href="https://github.com/thisisumarali" target="_blank">
+                        {/* <Link href="https://github.com/thisisumarali" target="_blank">
                             <Image
                                 src={GithubIcon}
                                 alt="Github Icon"
@@ -55,7 +55,7 @@ const Hero = () => {
                                 alt="Linked Icon"
                                 className="bg-gray-800 rounded-lg"
                             />
-                        </Link>
+                        </Link> */}
                     </div>
                 </div>
                 <div className='px-4 py-8 lg:py-0 lg:block flex justify-center'>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import GithubIcon from "../../../public/github-icon.svg"
-import LinkedinIcon from "../../../public/linkedin-icon.svg"
+// import GithubIcon from "../../../public/github-icon.svg"
+// import LinkedinIcon from "../../../public/linkedin-icon.svg"
 import Image from "next/image";
 import Link from "next/link"
 const Footer = () => {
@@ -11,7 +11,7 @@ const Footer = () => {
                     <span className='text-primary text-2xl'>Umar Khan</span>
                     <p className='text-slate-600 md:block hidden'>All right reserved </p>
                     <div className="gap-x-2 flex justify-center md:justify-start lg:gap-x-3">
-                        <Link href="https://github.com/thisisumarali" target="_blank">
+                        {/* <Link href="https://github.com/thisisumarali" target="_blank">
                             <Image
                                 src={GithubIcon}
                                 alt="Github Icon"
@@ -28,7 +28,7 @@ const Footer = () => {
                                 height={40} 
                                 className="bg-gray-800 rounded-lg"
                             />
-                        </Link>
+                        </Link> */}
                     </div>
                 </div>
             </div>
